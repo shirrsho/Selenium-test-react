f7da3386313dba2ad6b95233daee3a44ab2c7726
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-10-11 13:33:25 +0600

    signup written

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+
+  function isValidUsername(username){
+    if(username.length<3 || username.length>10) {alert("username must contain at least three, at most ten characters");return false;}
+    if(username[0]<'a'||username[0]>'z')
+      if(username[0]<'A'||username[0]>'Z')
+        {alert("username must start with a letter");return false}
+    for (let i = 0; i < username.length; i++) {
+      if(username[i]>='a' && username[i]<='z' || username[i]>='A' && username[i]<='Z' || username[i]>='0' && username[i]<='9') continue;
+      else {alert("only letters and numbers are allowed in username");return false;}
+    }
+    for (let i = 0; i < username.length; i++) {
+      if(username[i]>='a' && username[i]<='z' || username[i]>='A' && username[i]<='Z') return true
+    }
+    alert("only letter is not allowed in username")
+    return false
+  }
+  function isValidPassword(password){
+    if(password.length<5 || password.length>10) {alert("password must contain at least five, at most ten characters");return false;}
+    for (let i = 0; i < password.length; i++) {
+      if(password[i]>='a' && password[i]<='z' || password[i]>='A' && password[i]<='Z' || password[i]>='0' && password[i]<='9') continue;
+      else {alert("only letters and numbers are allowed in password");return false;}
+    }
+    let n = false; let l = false;
+    for (let i = 0; i < password.length; i++) {
+      if(password[i]>='a' && password[i]<='z' || password[i]>='A' && password[i]<='Z') l = true
+      if(password[i]>='0' && password[i]<='9') n = true
+    }
+    if(n && l) return true;
+    alert("must contain at least one letter, one number")
+    return false;
+  }
+  
+  let registerUser = (event)=> {
+    event.preventDefault();
+    console.log("in registerUser");
+    if(isValidUsername(event.target.username.value) && isValidPassword(event.target.password.value))
+      alert("Registered")
+  }
+
+  let loginUser = (event)=> {
+    event.preventDefault();
+    console.log("in loginUser");
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="Auth-form-container" style={{"width":"500px","margin":"auto","margin-top":"200px"}}>
+      <form className="Auth-form" onSubmit={registerUser}>
+        <div className="Auth-form-content" style={{"margin":"auto"}}>
+          <h3 className="Auth-form-title">Sign Up</h3>
+          
+          <div className="form-group mt-3">
+            <label>Username<p>at least three, at most ten characters.<br/>can contain letter and numbers. only number is not valid. must start with a letter</p></label>
+            <input
+              type="text"
+              name='username'
+              className="form-control mt-1"
+              placeholder="e.g shirsho_1106"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              name='email'
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password<p>at least five, at most ten characters.<br/>must contain at least one letter and one number.</p></label>
+            <input
+              type="password"
+              name='password'
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-  );
+
+    <div className="Auth-form-container" style={{"width":"500px","margin":"auto","margin-top":"200px","margin-bottom":"200px"}}>
+        <form className="Auth-form" onSubmit={loginUser}>
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+           
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                name='password'
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
