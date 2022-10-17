1b6a6e626a9ddc2551b4641e1be71d7a98306886
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-10-18 03:47:57 +0600

    done

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ const App = () => {
       if(username[0]<'A'||username[0]>'Z')
         {console.log("username must start with a letter");return false}
     for (let i = 0; i < username.length; i++) {
-      if(username[i]>='a' && username[i]<='z' || username[i]>='A' && username[i]<='Z' || username[i]>='0' && username[i]<='9') continue;
+      if((username[i]>='a' && username[i]<='z') || (username[i]>='A' && username[i]<='Z') || (username[i]>='0' && username[i]<='9')) continue;
       else {console.log("only letters and numbers are allowed in username");return false;}
     }
     for (let i = 0; i < username.length; i++) {
-      if(username[i]>='a' && username[i]<='z' || username[i]>='A' && username[i]<='Z') return true
+      if((username[i]>='a' && username[i]<='z') || (username[i]>='A' && username[i]<='Z')) return true
     }
     console.log("only letter is not allowed in username")
     return false
@@ -20,13 +20,13 @@ const App = () => {
   function isValidPassword(password){
     if(password.length<5 || password.length>10) {console.log("password must contain at least five, at most ten characters");return false;}
     for (let i = 0; i < password.length; i++) {
-      if(password[i]>='a' && password[i]<='z' || password[i]>='A' && password[i]<='Z' || password[i]>='0' && password[i]<='9') continue;
+      if((password[i]>='a' && password[i]<='z') || (password[i]>='A' && password[i]<='Z') || (password[i]>='0' && password[i]<='9')) continue;
       else {console.log("only letters and numbers are allowed in password");return false;}
     }
     let n = false; let l = false;
     for (let i = 0; i < password.length; i++) {
-      if(password[i]>='a' && password[i]<='z' || password[i]>='A' && password[i]<='Z') l = true
-      if(password[i]>='0' && password[i]<='9') n = true
+      if((password[i]>='a' && password[i]<='z') || (password[i]>='A' && password[i]<='Z')) l = true
+      if((password[i]>='0' && password[i]<='9')) n = true
     }
     if(n && l) return true;
     console.log("must contain at least one letter, one number")
@@ -44,7 +44,7 @@ const App = () => {
   let loginUser = (event)=> {
     event.preventDefault();
     console.log("in loginUser");
-    if(event.target.email.value=="" || event.target.password.value=="")
+    if(event.target.email.value==="" || event.target.password.value==="")
       alert("Login failed")
     else alert("Login Successfull")
   }
@@ -58,7 +58,7 @@ const App = () => {
           <h3 className="Auth-form-title">Sign Up</h3>
           
           <div className="form-group mt-3">
-            <label>Username<p>at least three, at most ten characters.<br/>can contain letter and numbers. only number is not valid. must start with a letter</p></label>
+            <label>Username<p style={{"font-size":"10px"}}>at least three, at most ten characters.<br/>can contain letter and numbers. only number is not valid. must start with a letter</p></label>
             <input
               type="text"
               name='username'
@@ -76,7 +76,7 @@ const App = () => {
             />
           </div>
           <div className="form-group mt-3">
-            <label>Password<p>at least five, at most ten characters.<br/>must contain at least one letter and one number.</p></label>
+            <label>Password<p style={{"font-size":"10px"}}>at least five, at most ten characters.<br/>must contain at least one letter and one number.</p></label>
             <input
               type="password"
               name='password'
